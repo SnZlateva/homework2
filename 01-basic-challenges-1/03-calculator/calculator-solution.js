9f8a1e9334ab507ddb82a3c505e2b792f9299b41
SnZlateva <127448@students.ue-varna.bg>
2025-03-07 18:39:18 +0200

    Update calculator-solution.js

--- 01-basic-challenges-1/03-calculator/calculator-solution.js
+++ 01-basic-challenges-1/03-calculator/calculator-solution.js
@@ -25,26 +25,26 @@ function calculator(num1, num2, operator) {
   return result;
 }
 
-// Solution 2
-function calculator(num1, num2, operator) {
-  // Declare a variable to store the result
-  let result;
+// // Solution 2
+// function calculator(num1, num2, operator) {
+//   // Declare a variable to store the result
+//   let result;
 
-  // Use if/else if/else statements to determine which operation to perform
-  if (operator === '+') {
-    result = num1 + num2;
-  } else if (operator === '-') {
-    result = num1 - num2;
-  } else if (operator === '*') {
-    result = num1 * num2;
-  } else if (operator === '/') {
-    result = num1 / num2;
-  } else {
-    // If the operator is not one of the above, throw an error
-    throw new Error('Invalid operator');
-  }
+//   // Use if/else if/else statements to determine which operation to perform
+//   if (operator === '+') {
+//     result = num1 + num2;
+//   } else if (operator === '-') {
+//     result = num1 - num2;
+//   } else if (operator === '*') {
+//     result = num1 * num2;
+//   } else if (operator === '/') {
+//     result = num1 / num2;
+//   } else {
+//     // If the operator is not one of the above, throw an error
+//     throw new Error('Invalid operator');
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
-module.exports = calculator;
+// module.exports = calculator;
